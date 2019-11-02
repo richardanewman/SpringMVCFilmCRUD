db44ad2613615e747b71ccc1401c5f763a4c2829
Richard Newman <17536735+richardanewman@users.noreply.github.com>
2019-11-02 09:04:55 -0600

    scales back files to work on MVP before implementing stretch goals such as validation

--- src/main/java/com/skilldistillery/film/data/AuthenticationDAOImpl.java
+++ src/main/java/com/skilldistillery/film/data/AuthenticationDAOImpl.java
@@ -1,51 +1,67 @@
-package com.skilldistillery.film.data;
-
-import java.util.HashMap;
-import java.util.Map;
-import com.skilldistillery.film.entities.*;
-public class AuthenticationDAOImpl implements AuthenticationDAO {
-	private Map<String, User> users = new HashMap<>();
-	
-	public AuthenticationDAOImpl () {
-		this.users.put("admin@admin.com", 
-				new User("admin@admin.com", "admin", "admin", "password", 22));
-	}
-
-	@Override
-	public User create(User user) {
-		if (isEmailUnique(user.getEmail())) {
-			users.put(user.getEmail(), user);
-			return user;
-		}
-		return null;
-	}
-
-	@Override
-	public boolean isEmailUnique(String email) {
-		// TODO: Check to see if the provided email exists as a key in the users HashMap
-	  return !users.containsKey(email);
-	}
-
-	@Override
-	public User getUserByEmail(String email) {
-		if (users.containsKey(email)) {
-			return users.get(email);
-		}
-		return null;
-	}
-
-	@Override
-	public boolean isValidUser(User u) {
-		if (getUserByEmail(u.getEmail()) == null) {
-			return false;
-		}
-		if (users.get(u.getEmail())
-				.getPassword()
-				.equals(u.getPassword())){
-			return true;
-		}
-		return false;
-	}
-
-	
-}
+//package com.skilldistillery.film.data;
+//
+//import com.skilldistillery.film.entities.User;
+//
+//public class AuthenticationDAOImpl implements AuthenticationDAO {
+//	private static final String URL = "jdbc:mysql://localhost:3306/sdvid?useSSL=false";
+//	private static final String USER = "student";
+//	private static final String PASSWORD = "student";
+//	
+//	
+//	static {
+//		try {
+//			Class.forName("com.mysql.jdbc.Driver");
+//		} catch (ClassNotFoundException e) {
+//			System.out.println("42");
+//			e.printStackTrace();
+//		}
+//	}
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	public AuthenticationDAOImpl () {
+//		this.users.put("admin@admin.com", 
+//				new User("admin@admin.com", "admin", "admin", "password", 22));
+//	}
+//
+//	@Override
+//	public User create(User user) {
+//		if (isEmailUnique(user.getEmail())) {
+//			users.put(user.getEmail(), user);
+//			return user;
+//		}
+//		return null;
+//	}
+//
+//	@Override
+//	public boolean isEmailUnique(String email) {
+//	  return !users.containsKey(email);
+//	}
+//
+//	@Override
+//	public User getUserByEmail(String email) {
+//		if (users.containsKey(email)) {
+//			return users.get(email);
+//		}
+//		return null;
+//	}
+//
+//	@Override
+//	public boolean isValidUser(User u) {
+//		if (getUserByEmail(u.getEmail()) == null) {
+//			return false;
+//		}
+//		if (users.get(u.getEmail())
+//				.getPassword()
+//				.equals(u.getPassword())){
+//			return true;
+//		}
+//		return false;
+//	}
+//
+//	
+//}
